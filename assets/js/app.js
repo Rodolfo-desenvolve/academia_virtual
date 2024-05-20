a30c990563dcd5be8947c26b139a1da63e05849a
Rodolfo bezerra <147216778+Rodolfo-desenvolve@users.noreply.github.com>
2024-05-20 07:48:40 -0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+var menu = document.getElementById('MenuItens')
+
+menu.style.maxHeight = '0px'
+
+function menucelular(){
+    if(menu.style.maxHeight == '0px'){
+        menu.style.maxHeight = '200px';
+    }else{
+        menu.style.maxHeight = '0px';
+    }
+}
+
+var produtoImg = document.getElementById('produtoImg')
+var produtoMiniatura = document.getElementsByClassName('produtoMiniatura') 
+
+produtoMiniatura[0].onclick = function(){
+    produtoImg.src = produtoMiniatura[0].src;
+}
+
+produtoMiniatura[1].onclick = function(){
+    produtoImg.src = produtoMiniatura[1].src;
+}
+
+produtoMiniatura[2].onclick = function(){
+    produtoImg.src = produtoMiniatura[2].src;
+}
+
+produtoMiniatura[3].onclick = function(){
+    produtoImg.src = produtoMiniatura[3].src;
+}
